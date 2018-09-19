1b942640634224c194e1848aaa1d49bf5720889d
zhangangs <zhangangs@163.com>
2018-09-19 11:07:05 +0800

    修改配置文件

--- themes/adoubi/source/js/web.js
+++ themes/adoubi/source/js/web.js
@@ -1,15 +1,15 @@
 window.onload = function () {
-    toHttps();
+    $.jumpToHttps();
 }
 
-/**
- * chrome浏览器中，会自动跳转到http开头，重定向到https
- */
-function toHttps() {
-    var proto = location.protocol.split(":")[0],
-        host = location.host,
-        reg = new RegExp(/zhangangs/);
-    if (proto == "http" && reg.test(host)) {
-        window.location.href = 'https://' + host;
+var $ = {
+    //重定向到https
+    jumpToHttps: function () {
+        var proto = location.protocol.split(":")[0],
+            host = location.host,
+            reg = new RegExp(/zhangangs/);
+        if (proto == "http" && reg.test(host)) {
+            window.location.href = 'https://' + host;
+        }
     }
 }
